28c8c2f9819452ebe6bda70acb08b4e5c26e8b96
Anthony <anthonycnoble@live.com>
2019-10-18 18:14:50 -0700

    initial commit bootstrap menu + Carousel

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NobleHawkDevelopment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
